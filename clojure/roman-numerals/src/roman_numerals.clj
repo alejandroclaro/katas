2a5b023e440fde2ebc1ad5be3787f27d9099f31a
Alejandro Claro <alejandro.claro@smartmatic.com>
2014-10-31 21:07:12 -0430

    * Minor refactor to clojure roman numerals kata.

--- clojure/roman-numerals/src/roman_numerals.clj
+++ clojure/roman-numerals/src/roman_numerals.clj
@@ -1,16 +1,16 @@
 ; Roman numerals code kata.
 (ns roman-numerals)
 
-(def alphabet
-  (sorted-map-by > 1000 "M" 900 "CM" 500 "D" 400 "CD"
-                    100 "C"  90 "XC"  50 "L"  40 "XL"
-                     10 "X"   9 "IX"   5 "V"   4 "IV"
-                      1 "I"   0 ""))
+(def alphabet (sorted-map-by > 1000 "M" 900 "CM" 500 "D" 400 "CD"
+                                100 "C"  90 "XC"  50 "L"  40 "XL"
+                                 10 "X"   9 "IX"   5 "V"   4 "IV"
+                                  1 "I"   0 ""))
 
-(defn to-roman-step [x result]
-  (if (= x 0)
-    result
-    (let [[k v] (some #(when (>= x (key %)) %) alphabet)] (to-roman-step (- x k) (str result v)))))
+(defn find-next-digit [x]
+  (some #(when (>= x (key %)) %) alphabet))
 
 (defn to-roman [x]
-  (to-roman-step x ""))
+  (loop [n x result ""]
+    (if (> n 0)
+      (let [[k v] (find-next-digit n)] (recur (- n k) (str result v)))
+      result)))
