a301e68fb7897933df1e237d8e7c920f777e82ce
Alejandro Claro <alejandro.claro@smartmatic.com>
2014-10-31 21:18:42 -0430

    * Minor clojure roman numerals kara chage.

--- clojure/roman-numerals/src/roman_numerals.clj
+++ clojure/roman-numerals/src/roman_numerals.clj
@@ -6,11 +6,11 @@
                                  10 "X"   9 "IX"   5 "V"   4 "IV"
                                   1 "I"   0 ""))
 
-(defn find-next-symbol [x]
+(defn find-closest-symbol [x]
   (some #(when (>= x (key %)) %) alphabet))
 
 (defn to-roman [x]
   (loop [n x result ""]
     (if (> n 0)
-      (let [[value symbol] (find-next-symbol n)] (recur (- n value) (str result symbol)))
+      (let [[value symbol] (find-closest-symbol n)] (recur (- n value) (str result symbol)))
       result)))
